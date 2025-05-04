8b73242c97c896289f66424c99f5092136e9f7d8
Isah Idris <idris.isah2@st.futminna.edu.ng>
2025-05-05 00:07:27 +0100

    infrastructure with pulumi: abstraction of subnets creations

--- infra/main.go
+++ infra/main.go
@@ -11,10 +11,27 @@ func main() {
 
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		err := vpc.CreateVpc(ctx, vpc.Parameters{})
+		Vpc, err := vpc.CreateVpc(ctx, vpc.Parameters{})
 		if err != nil {
 			return fmt.Errorf("failed to create VPC: %v", err)
 		}
+		validConfig, err := vpc.Validate(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to load configurations: %w", err)
+		}
+		subnets, err := vpc.CreateSubnets(ctx, Vpc, validConfig)
+		if err != nil {
+			return fmt.Errorf("failed to create subnets: %v", err)
+		}
+
+		ctx.Export("VpcId", Vpc.ID())
+		ctx.Export("VpcCidr", Vpc.CidrBlock)
+		ctx.Export("PublicSubnetIds", subnets[0].ID())
+		ctx.Export("PrivateSubnetIds", subnets[1].ID())
+		ctx.Export("PublicSubnetAz", subnets[0].AvailabilityZone)
+		ctx.Export("PrivateSubnetAz", subnets[1].AvailabilityZone)
+		ctx.Export("PublicSubnetCidr", subnets[0].CidrBlock)
+		ctx.Export("PrivateSubnetCidr", subnets[1].CidrBlock)
 
 		return nil
 	})
